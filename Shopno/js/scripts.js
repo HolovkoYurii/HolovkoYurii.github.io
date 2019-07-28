aeee6424c09926974b1824e1424968b82e4f9a6f
Holoko Yurii <golovko.jura.g@gmail.com>
2019-07-28 23:55:54 +0300

    portfolio

---
+++ Shopno/js/scripts.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $('.carousel').slick({
+      dots:true,
+      autoplay: true,
+      autoplaySpeed:5000,	
+      infinite: true,
+      slidesToShow: 1,
+      slidesthoshow:3,
+      
+    });
+});
+
+$(document).ready(function(){
+  $('.team-carousel').slick({
+    dots:true,
+    autoplay: true,
+    autoplaySpeed:5000,	
+    infinite: true,
+    slidesToShow: 1,
+    slidesthoshow:3,
+    
+  });
+});
